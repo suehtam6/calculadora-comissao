a969913613ca6b71a4bf3a03824220456fdf9ba2
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2025-09-15 16:58:57 -0300

    first commit

---
+++ src/br/senai/sp/jandira/ds1t/comissoes/Comissoes/ComissoesApp.java
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.ds1t.comissoes.Comissoes;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Ininciando o programa...");
+
+        
+    }
+
+
+}
---
+++ src/br/senai/sp/jandira/ds1t/comissoes/Comissoes/model/Vendedor.java
@@ -0,0 +1,4 @@
+package br.senai.sp.jandira.ds1t.comissoes.Comissoes.model;
+
+public class Vendedor {
+}
