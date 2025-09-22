83a3b3c4eddd82f9138213033dfd102b7bf7e20e
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2025-09-22 13:05:32 -0300

    Final do trabalho

--- src/br/senai/sp/jandira/ds1t/comissoes/Comissoes/ComissoesApp.java
+++ src/br/senai/sp/jandira/ds1t/comissoes/Comissoes/ComissoesApp.java
@@ -1,11 +1,16 @@
 package br.senai.sp.jandira.ds1t.comissoes.Comissoes;
 
-public class App {
+import br.senai.sp.jandira.ds1t.comissoes.Comissoes.model.Vendedor;
+
+public class ComissoesApp {
 
     public static void main(String[] args) {
         System.out.println("Ininciando o programa...");
 
-        
+
+        Vendedor vendedor = new Vendedor();
+        vendedor.coletarDados();
+
     }
 
 
--- src/br/senai/sp/jandira/ds1t/comissoes/Comissoes/model/Vendedor.java
+++ src/br/senai/sp/jandira/ds1t/comissoes/Comissoes/model/Vendedor.java
@@ -1,4 +1,68 @@
 package br.senai.sp.jandira.ds1t.comissoes.Comissoes.model;
 
+
+import java.util.Scanner;
+
 public class Vendedor {
+
+    String nomeVendedor;
+    double totalVendas;
+    double percentualComissao;
+    double valorComissao;
+
+    public void coletarDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        //obtendo os dados dos vendedores
+        System.out.println("** CALCULADORA DE COMISSÕES**");
+        System.out.println("-------------------------------");
+
+        //obtendo o nome do vendedor
+        System.out.print("Nome do vendedor: ");
+        nomeVendedor = leitor.nextLine();
+
+        //obtendo o total de vendas do vendedor
+        System.out.println("Total de vendas do(a) " + nomeVendedor + ":");
+        totalVendas = leitor.nextDouble();
+
+    calcularComissao();
+
+    }
+
+
+    public void calcularComissao(){
+
+        if (totalVendas < 20000){
+            percentualComissao = 5;
+
+        } else {
+            percentualComissao = 10;
+
+        }
+
+        valorComissao = totalVendas * percentualComissao / 100;
+
+        exibirResultados();
+    }
+
+    public void exibirResultados(){
+
+
+        String resultadoComDoisValores = String.format("%.2f", valorComissao);
+
+        System.out.println("***************************");
+        System.out.println("NOME DO VENDEDOR: " + nomeVendedor);
+        System.out.println("TOTAL DE VENDAS: " + totalVendas);
+        System.out.println("PERCENTUAL DE COMISSÃO: " + percentualComissao);
+        System.out.println("VALOR DA COMISSÃO: R$" + resultadoComDoisValores);
+        System.out.println("***************************");
+
+
+
+
+    }
+
+
+
+
 }
